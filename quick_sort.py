ec6f0621092aeaef21f3b98f0aea6260c91407b9
Ramiro B. Vázquez <ramirovazquez95@gmail.com>
2019-11-05 10:44:07 -0600

    quick sort working

--- quick_sort.py
+++ quick_sort.py
@@ -2,13 +2,11 @@
 QUICK SORT PSEUDOCODE
 quicksort(A)
     quicksort(A, 0, len(A)-1)
-
 quicksort(A, lo, hi)
-    if (hi >= lo) return 
+    if (lo >= hi) return 
     pivot_location <- partition(A, lo, hi)
     quicksort(A, lo, pivol_location-1)
     quicksort(A, pivol_location+1, hi)
-
 partition(A, lo, hi)
     pivot_index <- random(lo, hi)
     swap(A, pivot_index, hi)
@@ -21,26 +19,76 @@ partition(A, lo, hi)
             else:
                 C[j--] <- A[k]
             k++
-        C[i] <- A[hi] (copy the pivot in)
-        copy C[lo:hi] back into A
-        return i
-
-    
-    
+    C[i] <- A[hi] (copy the pivot in)
+    copy C[lo:hi] back into A
+    return i
 '''
 from merge import merge, mergethree
+import random
+def partition(A):
+    C     = []
+    left  = []
+    right = []
+
+    if len(A) > 0:
+        pivot_index = random.randint(0,len(A)-1)
+        C = ["*"] * len(A)
+        pivot = A.pop(pivot_index)
+        #print("... PIVOT {}".format(pivot))
+        for k in A:
+            if k <= pivot:
+                left.append(k)
+            else:
+                right.append(k)
+        C = left + [pivot] + right
+    return len(left), C
 
 def quicksort_(A):
-    return []
+    if len(A) <= 1:
+        #print("condicion base ...{}".format(A))
+        return A
+    pivot_location, A = partition(A)
+    left = quicksort_(A[:pivot_location])
+    right = quicksort_(A[pivot_location:])
+    return left + right
 
 def quicksort(A=[12,11,13,5,6,7,1,9,8,15]): # son 10 elementos
     return quicksort_(A)
 
-
-
 if __name__ == "__main__":
+    # partition, working
+    print("------------------------------------------------------------")
+    print("------------------------PARTITION---------------------------")
+    
     z = [12,11,13,5,6,7,1,9,8,15]
+    index_pivot, z = partition(z)
+    print("index_pivot: {} and z .... {}".format(index_pivot,z))
 
+    index_pivot, z = partition([5,1,6])
+    print("index_pivot: {} and z .... {}".format(index_pivot,z))
+
+    index_pivot, z = partition([5,1])
+    print("index_pivot: {} and z .... {}".format(index_pivot,z))
+
+    index_pivot, z = partition([5])
+    print("index_pivot: {} and z .... {}".format(index_pivot,z))
+
+    index_pivot, z = partition([])
+    print("index_pivot: {} and z .... {}".format(index_pivot,z))
+
+    print("-------------------------ENDPARTITION------------------------")
+    print("-------------------------------------------------------------")
+    
+    z = [2,8,7,4,1,0,9,5,3]
+    print("original ...................... z")
+    print(z)
+    answer_a = quicksort(z)
+    print("quicksort ...................... answer")
+    print(answer_a)
+
+    z = [12,11,13,5,6,7,1,9,8,15]
+    print("original ...................... z")
+    print(z)
     answer_a = quicksort(z)
     print("quicksort ...................... answer")
     print(answer_a)
