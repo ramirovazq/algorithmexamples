a04d4d068c3f052a24c2d82658f0e4cb8d5da071
Ramiro B. Vázquez <ramirovazquez95@gmail.com>
2019-11-07 10:40:05 -0600

    extract all elements

--- heap_extract_min.py
+++ heap_extract_min.py
@@ -24,11 +24,16 @@ extract-min(A, k)
 from quick_sort import partition
 
 def sink(A, k):
+    #print(A)
     N = len(A)
+    #print(N)
     while (2*k) <= N:
         smallest = (2*k) - 1               # we suppose smallest is left child
-        if (A[(2*k)-1] > A[(2*k) + 1 - 1]): # compares, if left child is greater than right child 
-            smallest = (2 * k) + 1 - 1      # then right is smallest
+        try:
+            if (A[(2*k)-1] > A[(2*k) + 1 - 1]): # compares, if left child is greater than right child 
+                smallest = (2 * k) + 1 - 1      # then right is smallest
+        except IndexError: # doesnt have right child
+            pass
         if A[k-1] < A[smallest]:            # if A[k] element is smaller than smallest, we finish
             break
 
@@ -46,15 +51,35 @@ def sink(A, k):
 def extract_min(A=[2,3,6,7,10,8]): # son 6 elementos
     N = len(A)
     minimo = A[0]
-    A[0] = A[N-1]
-    A.pop() # quit last element 
+    A[0] = A[N-1] # change last element to first element
+    A.pop() # quit last element  (that is min)
     
     C = sink(A,1)
 
-    assert C == [3,7,6,8,10], ('not final result correct')
-    return minimo
+    return minimo, C
 
 if __name__ == "__main__":
-
-    answer_a = extract_min()
+    
+    answer_a, newarray = extract_min()
     assert answer_a == 2, ("no encuentra el elemento correcto")
+    assert newarray == [3,7,6,8,10], ('not final result correct')
+
+    answer_a, newarray = extract_min(newarray)
+    assert answer_a == 3, ("no encuentra el elemento correcto")
+    assert newarray == [6,7,10,8], ('not final result correct')
+
+    answer_a, newarray = extract_min(newarray)
+    assert answer_a == 6, ("no encuentra el elemento correcto")
+    assert newarray == [7,8,10], ('not final result correct')
+    
+    answer_a, newarray = extract_min(newarray)
+    assert answer_a == 7, ("no encuentra el elemento correcto")
+    assert newarray == [8,10], ('not final result correct')
+    
+    answer_a, newarray = extract_min(newarray)
+    assert answer_a == 8, ("no encuentra el elemento correcto")
+    assert newarray == [10], ('not final result correct')
+    
+    answer_a, newarray = extract_min(newarray)
+    assert answer_a == 10, ("no encuentra el elemento correcto")
+    assert newarray == [], ('not final result correct')
