c6691225108481e3e2a3d4b54026ee5ddcedc0ea
Ramiro B. Vázquez <ramirovazquez95@gmail.com>
2019-11-11 10:00:29 -0600

    url for max number of toys

--- example_algorithms/max_buy_toys.py
+++ example_algorithms/max_buy_toys.py
@@ -1,3 +1,9 @@
+'''
+
+url
+https://www.hackerrank.com/challenges/mark-and-toys/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=sorting
+'''
+
 def maximumToys(prices, k):
     prices.sort()
     count = 0
