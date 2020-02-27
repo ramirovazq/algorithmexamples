8498b01f6a417210a9b34d006aeb528955233160
Ramiro Baltazar Vazquez Sanchez <ramirovazquez95@gmail.com>
2020-02-27 07:11:39 -0600

    now are grouped

--- example_algorithms/2d_array_ds.py
+++ example_algorithms/2d_array_ds.py
@@ -12,13 +12,13 @@ def check3(list_of_elements, indice=0):
 	check if one element have
 	next 2 subsecuent elements
 	'''
-	answer = False
+	answer = [[],[],[]]
 	try:
 		# necessary to exist actual row, and two next rows
 		element = list_of_elements[indice] 
 		middle_element = list_of_elements[indice+1] 
 		last_element   = list_of_elements[indice+2]
-		answer = True
+		answer = [element,middle_element,last_element]
 	except IndexError:
 		pass
 	return answer
@@ -27,11 +27,31 @@ def check3(list_of_elements, indice=0):
 def hourglassSum(arr):
 	print(arr)
 	for indice, row in enumerate(arr):
-		if check3(arr, indice):
-			print(indice, row)
-			for indice2, element in enumerate(row):
-				if check3(row, indice2):
-					print(indice2)
+		list_with_lists = check3(arr, indice)
+		if any(list_with_lists):
+			print("___________ini")
+			
+			first_list  = list_with_lists[0]
+			middle_list = list_with_lists[1]
+			last_list   = list_with_lists[2]
+			print(".....first_list, middle_list, last_list...")
+			print(first_list, middle_list, last_list)
+
+			for indice_element, element in enumerate(first_list):
+				group_first  = check3(first_list, indice_element)
+				group_middle = check3(middle_list, indice_element)
+				group_last   = check3(last_list, indice_element)
+
+				print(".....group_three...ini")
+				print(group_first)
+				print(group_middle)
+				print(group_last)
+				print(".....group_three...fin")
+
+			print("___________fin")
+
+
+
 		else:
 			break
 		
